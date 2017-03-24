5c2b5a3d7f786ee61c5fe5d3771f7687917edc0a
Richard Wilbur <richard.wilbur@hotmail.com>
2017-03-23 23:14:59 -0700

    Update port for heroku

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+var PORT = 3000;
 
-app.get('/', function (req, res) {
-	res.send('Hello world!');
-});
+var middleware = require('./middleware.js');
 
-app.get('/about', function (req, res) {
+app.use(middleware.logger);
+
+// app.get('/', function (req, res) {
+// 	res.send('Hello world!');
+// });
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
 	res.send('About Us!');
 });
 
-app.listen(port, function () {
-	console.log('Server started on port ' + port);
+app.use(express.static(__dirname + '/public)'));
+
+app.listen(PORT, function () {
+	console.log('Server started on port ' + PORT);
 });
